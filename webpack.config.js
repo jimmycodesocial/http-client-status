c84666ee72b2830c96b27de8695d47dcd242c7f2
Alberto Menendez Romero <albertomr86@gmail.com>
2017-10-23 16:46:11 -0300

    Compiling.

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+/**
+ * Copyright(c) 2016 JimmyCode Social <hi@jimmycode.com> (https://jimmycode.com)
+ */
+
+'use strict';
+
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'HttpClientStatus',
+    libraryTarget: 'umd'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compressor: {
+    //     warnings: false
+    //   }
+    // })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: path.join(__dirname, 'src'),
+      }
+    ]
+  }
+};
